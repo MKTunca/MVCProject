19634a64f8ac87a023c9c2f816f74c68c19f7007
MKTunca <91753517+MKTunca@users.noreply.github.com>
2021-12-22 19:02:06 +0300

    Add files via upload

---
+++ Models/siteBilgileri.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TatooRentACar.Models
+{
+    public class siteBilgileri
+    {
+        RentACarEntities baglan = new RentACarEntities();
+        public List<AKategori> anakategori { get; set; }
+        public List<Bkategori> altkategori { get; set; }
+        public List<ARAC> araclar { get; set; }
+        public List<ARAC> aracFiltre { get; set; }
+        public Int32 aracsayisi { get; set; }
+        
+    
+        
+        public void bilgiler()
+        {
+            anakategori = baglan.AKategori.ToList();
+            altkategori = baglan.Bkategori.ToList();
+            araclar = baglan.ARAC.Where(m => m.durum == "Aktif").ToList();
+            aracsayisi = baglan.ARAC.Where(m => m.durum == "Aktif").Count();
+            
+        }
+    }
+}
---
+++ fiyatHesap.js
@@ -0,0 +1,16 @@
+﻿function hesapla(h_id) {
+    var gun = document.getElementById('gun_' + h_id);
+    var sonuc = document.getElementById('sonuc_' + h_id);
+    var fiyat = document.getElementById('fiyat_' + h_id);
+    sonuc.value = gun.value * fiyat.value;
+    
+    if (gun.value <= 7) {
+       
+       bootbox.alert('sonuc=' + sonuc.value + 'TL');
+            
+    }
+    else {
+        bootbox.alert('7 günden fazla araç kiralayamazsınız.')
+    }
+    
+}
